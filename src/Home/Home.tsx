b9fe5c57d985acc9a0603537d184e82f9a6754d1
Rodrigo Alberto Villablanca <138055427+shodrig0@users.noreply.github.com>
2025-08-05 19:25:40 -0300

    Update Home.tsx
    important

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -6,10 +6,14 @@ const Home = () => {
 
     return (
         <CalendarContextProvider>
-
+            
             <Header />
             <Dashboard />
-
+            <h3>Yep, this project is not complete yet</h3>
+            <h3>I'm working on it to understand the logic behind libs such as 'date-fns' and 'Material UI'</h3>
+            <h3>Also, to learn how to use the “useContext” hook</h3>
+            <h4>The next step would be to be able to add tasks with a date and time</h4>
+            <h4>So, this is still a work in progress!</h4>
         </CalendarContextProvider>
     )
 }
